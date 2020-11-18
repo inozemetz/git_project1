ae0839f0b45e34a05c5c864d70f6df6923eb9a9a
Vanya <slavamateshe@yandex.ru>
2020-11-18 16:07:08 +0300

    bruh

--- auf.py
+++ auf.py
@@ -1,7 +1,9 @@
 def main():
-    print('my first git program')
+    print('My first git program')
     print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
